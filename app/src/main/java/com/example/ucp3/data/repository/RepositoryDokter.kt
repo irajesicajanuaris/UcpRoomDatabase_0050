b1747683626319afadd1a57c7cd1608b3eca8892
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-19 20:04:11 +0700

    menambahkan repository dan Localrepository Dokter

---
+++ app/src/main/java/com/example/ucp3/data/repository/RepositoryDokter.kt
@@ -0,0 +1,11 @@
+package com.example.ucp3.data.repository
+
+import com.example.ucp3.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryDokter {
+    suspend fun insertDokter(dokter: Dokter)
+
+    fun getAllDokter(): Flow<List<Dokter>>
+    fun getDokter(idDokter: String): Flow<Dokter>
+}
