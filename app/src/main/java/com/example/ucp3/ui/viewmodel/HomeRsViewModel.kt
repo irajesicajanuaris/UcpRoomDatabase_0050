b3565bbac2ae245498f7dfb3bb4ef7e40bd848c8
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-19 22:06:44 +0700

    menambahkan menambahkan class HomeRsViewModel

--- app/src/main/java/com/example/ucp3/ui/viewmodel/HomeRsViewModel.kt
+++ app/src/main/java/com/example/ucp3/ui/viewmodel/HomeRsViewModel.kt
@@ -1,6 +1,51 @@
 package com.example.ucp3.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp3.data.entity.Dokter
+import com.example.ucp3.repository.RepositoryDokter
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeRsViewModel (
+    private val repositoryDokter: RepositoryDokter
+): ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryDokter.getAllDokter()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeUiState(
     val listMhs: List<Dokter> = listOf(),
