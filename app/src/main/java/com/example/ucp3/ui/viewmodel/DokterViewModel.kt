6841d61ee527f9dd1815c5e26573d09482ff515b
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-20 17:55:17 +0700

    menambahkan class DokterViewModel dalam DokterViewModel.kt

--- app/src/main/java/com/example/ucp3/ui/viewmodel/DokterViewModel.kt
+++ app/src/main/java/com/example/ucp3/ui/viewmodel/DokterViewModel.kt
@@ -1,6 +1,74 @@
 package com.example.ucp3.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp3.data.entity.Dokter
+import com.example.ucp3.repository.RepositoryDokter
+import kotlinx.coroutines.launch
+
+class DokterViewModel(
+    private val repositoryDokter: RepositoryDokter
+) : ViewModel(){
+
+    var uiState by mutableStateOf(DokterUIState())
+
+    //memperbarui state berdasarkan input pengguna
+    fun updateState(DokterEvent: DokterEvent){
+        uiState = uiState.copy(
+            dokterEvent = DokterEvent,
+        )
+    }
+
+    //validasi data input pengguna
+    private fun validateFields(): Boolean{
+        val event = uiState.dokterEvent
+        val errorState = FormErrorState(
+            idDokter = if (event.idDokter.isNotEmpty()) null else "Id Dokter tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            spesialis = if (event.spesialis.isNotEmpty()) null else "Spesialis tidak boleh kosong",
+            klinik = if (event.klinik.isNotEmpty()) null else "Klinik tidak boleh kosong",
+            noHp = if (event.noHp.isNotEmpty()) null else "No Hp tidak boleh kosong",
+            jamKerja = if (event.jamKerja.isNotEmpty()) null else "Jam Kerja tidak boleh kosong",
+
+            )
+
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isvalid()
+    }
+
+    //menyimpan data ke repository
+    fun saveData() {
+        val currentEvent = uiState.dokterEvent
+
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositoryDokter.insertDokter(currentEvent.toDokterEntity())
+                    uiState = uiState.copy(
+                        snackbarMessage = "Data Berhasil disimpan",
+                        dokterEvent = DokterEvent(), // Reset input form
+                        isEntryValid = FormErrorState() // Reset error state
+                    )
+                } catch (e: Exception){
+                    uiState = uiState.copy(
+                        snackbarMessage = "Data gagal disimpan"
+                    )
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackbarMessage = "Input tidak valid. Periksa kelmbali data Anda"
+            )
+        }
+    }
+    // reset pesan snackbar setelah ditampilkan
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(snackbarMessage = null)
+    }
+}
 
 
 data class DokterUIState(
