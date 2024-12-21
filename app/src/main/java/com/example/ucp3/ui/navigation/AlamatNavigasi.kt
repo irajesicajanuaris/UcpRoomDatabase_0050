24a7d11c38f6e2c1546a27c02495ffac0cea73bc
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-21 15:58:16 +0700

    fix alamat navigasi

--- app/src/main/java/com/example/ucp3/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp3/ui/navigation/AlamatNavigasi.kt
@@ -8,3 +8,15 @@ interface AlamatNavigasi {
 object DestinasiHome : AlamatNavigasi{
     override val route = "Home"
 }
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val IdJadwal = "idJadwal"
+    val routesWithArg = "$route/{$IdJadwal}"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val IdJadwal = "idJadwal"
+    val routesWithArg = "$route/{$IdJadwal}"
+}
