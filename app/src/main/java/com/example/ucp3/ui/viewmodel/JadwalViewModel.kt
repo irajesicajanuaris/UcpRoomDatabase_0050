80dbad1ef073d0252db2f588c7dec97525005de2
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-21 15:17:22 +0700

    menambahkan data class FormJadwalErrorState dan JadwalUIState

--- app/src/main/java/com/example/ucp3/ui/viewmodel/JadwalViewModel.kt
+++ app/src/main/java/com/example/ucp3/ui/viewmodel/JadwalViewModel.kt
@@ -5,6 +5,32 @@ import com.example.ucp3.data.entity.Jadwal
 class JadwalViewModel {
 
 }
+
+
+data class JadwalUIState(
+    val JadwalEvent: JadwalEvent = JadwalEvent(),
+    val isEntryValid: FormJadwalErrorState = FormJadwalErrorState(),
+    val snackbarMessage: String? = null,
+)
+
+data class FormJadwalErrorState(
+    val idJadwal: String? = null,
+    val namaDokter: String? = null,
+    val namaPasien: String? = null,
+    val noHpPasien: String? = null,
+    val tanggalKonsultasi: String? = null,
+    val status: String? = null,
+){
+    fun isvalid(): Boolean{
+        return idJadwal == null
+                && namaDokter == null
+                && namaPasien == null
+                && noHpPasien == null
+                && tanggalKonsultasi == null
+                && status == null
+    }
+}
+
 data class JadwalEvent(
     val idJadwal: String = "",
     val namaDokter: String = "",
