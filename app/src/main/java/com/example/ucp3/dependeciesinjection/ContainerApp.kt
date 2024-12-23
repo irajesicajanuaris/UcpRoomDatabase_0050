6d0ea1753e75d1b741c216b581ec75fd616225dd
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-24 01:04:52 +0700

    menggabungkan Repository Dokter dan Repository Jadwal menjadi Repository.kt

--- app/src/main/java/com/example/ucp3/dependeciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp3/dependeciesinjection/ContainerApp.kt
@@ -2,22 +2,17 @@ package com.example.ucp3.dependeciesinjection
 
 import android.content.Context
 import com.example.ucp3.data.database.RsDatabase
-import com.example.ucp3.repository.LocalRepositoryDokter
-import com.example.ucp3.repository.LocalRepositoryJadwal
-import com.example.ucp3.repository.RepositoryDokter
-import com.example.ucp3.repository.RepositoryJadwal
+import com.example.ucp3.repository.LocalRepository
+import com.example.ucp3.repository.Repository
 
 interface InterfaceContainerApp {
-    val repositoryDokter: RepositoryDokter
-    val repositoryJadwal: RepositoryJadwal
+    val repository: Repository
 }
 
 class ContainerApp(private val context: Context) : InterfaceContainerApp{
-    override val repositoryDokter by lazy {
-        LocalRepositoryDokter(RsDatabase.getDatabase(context).dokterDao())
-    }
-    override val repositoryJadwal by lazy {
-        LocalRepositoryJadwal(RsDatabase.getDatabase(context).jadwalDao())
+    override val repository by lazy {
+        LocalRepository(RsDatabase.getDatabase(context).dokterDao(),
+            RsDatabase.getDatabase(context).jadwalDao())
     }
 }
 
--- app/src/main/java/com/example/ucp3/repository/Repository.kt
+++ app/src/main/java/com/example/ucp3/repository/Repository.kt
@@ -1,12 +1,17 @@
 package com.example.ucp3.repository
 
+import com.example.ucp3.data.entity.Dokter
 import com.example.ucp3.data.entity.Jadwal
 import kotlinx.coroutines.flow.Flow
 
-interface RepositoryJadwal {
+interface Repository {
+    suspend fun insertDokter(dokter: Dokter)
+
+    fun getAllDokter(): Flow<List<Dokter>>
+
     suspend fun insertJadwal(jadwal: Jadwal)
     fun getAllJadwal(): Flow<List<Jadwal>>
-    fun getJadwal(idJadwal: String): Flow<Jadwal>
+    fun getJadwal(idJadwal: Int): Flow<Jadwal>
     suspend fun deleteJadwal(jadwal: Jadwal)
     suspend fun updateJadwal(jadwal: Jadwal)
 }
