c2ca1c226f0a0c1a4df4cdfef42305bb10f5ff9b
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-24 01:05:54 +0700

    menggabungkan LocalRepository Dokter dan LocalRepository Jadwal menjadi Repository.kt

--- app/src/main/java/com/example/ucp3/repository/LocalRepository.kt
+++ app/src/main/java/com/example/ucp3/repository/LocalRepository.kt
@@ -1,12 +1,23 @@
 package com.example.ucp3.repository
 
+import com.example.ucp3.data.dao.DokterDao
 import com.example.ucp3.data.dao.JadwalDao
+import com.example.ucp3.data.entity.Dokter
 import com.example.ucp3.data.entity.Jadwal
 import kotlinx.coroutines.flow.Flow
 
-class LocalRepositoryJadwal(
+class LocalRepository (
+    private val dokterDao: DokterDao,
     private val jadwalDao: JadwalDao
-) : RepositoryJadwal {
+) : Repository {
+
+    override suspend fun insertDokter(dokter: Dokter) {
+        dokterDao.insertDokter(dokter)
+    }
+
+    override fun getAllDokter(): Flow<List<Dokter>> {
+        return dokterDao.getAllDokter()
+    }
 
     override suspend fun insertJadwal(jadwal: Jadwal) {
         jadwalDao.insertJadwal(jadwal)
@@ -16,7 +27,7 @@ class LocalRepositoryJadwal(
         return jadwalDao.getAllJadwal()
     }
 
-    override fun getJadwal(idJadwal: String): Flow<Jadwal> {
+    override fun getJadwal(idJadwal: Int): Flow<Jadwal> {
         return jadwalDao.getJadwal(idJadwal)
     }
 
