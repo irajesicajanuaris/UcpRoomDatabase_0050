5b110a98eb4e9667ad6168ab843004616861255d
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-24 01:14:06 +0700

    menambahkan TopAppBar

--- app/src/main/java/com/example/ucp3/ui/costumewidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp3/ui/costumewidget/TopAppBar.kt
@@ -1,5 +1,6 @@
 package com.example.ucp3.ui.costumewidget
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,11 +11,16 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp3.R
 
 
+@Preview(showBackground = true)
 @Composable
 fun TopAppBar(
     onBack: () -> Unit,
@@ -25,30 +31,32 @@ fun TopAppBar(
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center // Pastikan konten di tengah
+            .background(color = colorResource(id = R.color.teal_700))
+            .padding(10.dp),
+        contentAlignment = Alignment.Center
     ) {
         if (showBackButton) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.CenterVertically,
             ) {
                 TextButton(
                     onClick = onBack,
                     modifier = Modifier.align(Alignment.CenterVertically)
                 ) {
-                    Text("Kembali")
+                    Text("Kembali",
+                        color = Color.White )
                 }
                 Spacer(modifier = Modifier.weight(2f))
             }
         }
 
-        // Teks judul
         Text(
             text = judul,
-            fontSize = 25.sp,
+            fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(Alignment.Center)
+            color = Color.White,
+            modifier = Modifier.align(Alignment.Center).padding(25.dp)
         )
     }
 }
